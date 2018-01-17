b38cab373261f0fb289ffa5d3a665cb41c64a2fa
jerj <johnbrz@gmail.com>
2018-01-17 15:33:52 -0800

    generated 4 page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { SetupComponent } from './setup/setup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    GraphsComponent,
+    SetupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
