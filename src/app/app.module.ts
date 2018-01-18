610825d20cb49e3cb75b00e044202e78c9190597
Matt Mikolajczyk <mmikolajczyk1111@gmail.com>
2018-01-18 15:59:47 -0800

    add text boxes, made exercise object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { GraphsComponent } from './graphs/graphs.component';
@@ -16,7 +16,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Exercise } from '../exercise';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
+  pushups: Exercise = {
+    id: 1,
+    ask: 'How many push-ups did you do today?',
+    target: 0,
+    actual: 0
+  };
+  
+  miles: Exercise = {
+    id: 2,
+    ask: 'How many miles did you run today?',
+    target: 0,
+    actual: 0
+  };
 
   constructor() { }
 
